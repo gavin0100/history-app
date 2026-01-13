377cfa434f7e1d6b96692749dcfdf0151f73ddaa
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2026-01-13 23:35:13 +0700

    #SCRUM-3: Set up layout for application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      // Your feature routes here
+      { path: '', redirectTo: 'articles', pathMatch: 'full' },
+      // Add your article, admin routes etc.
+    ],
+  },
+];
